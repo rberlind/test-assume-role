e221844215058e64e99c5ad936438f1b905132ce
Roger Berlind <rberlind@optonline.net>
2018-04-12 20:17:43 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,34 +8,34 @@ resource "null_resource" "write_credentials" {
   }
 
   provisioner "local-exec" {
-    command = "aws sts assume-role --role-arn=arn:aws:iam::350965860658:role/ecs-nonprod-secrets-test-role --role-session-name='TFE-Apply' --region=us-east-1 >> /tmp/json_credentials"
+    command = "aws sts assume-role --role-arn=arn:aws:iam::128997349609:role/terraform-role --role-session-name='TFE-Apply' --region=us-east-1 >> json_credentials"
   }
 
   provisioner "local-exec" {
-    command = "cat /tmp/credentials | jq --exit-status --raw-output .Credentials.AccessKeyId >> /tmp/aws_access_key_id"
+    command = "cat /tmp/credentials | jq --exit-status --raw-output .Credentials.AccessKeyId >> aws_access_key_id"
   }
 
   provisioner "local-exec" {
-    command = "cat /tmp/credentials | jq --exit-status --raw-output .Credentials.SecretAccessKey >> /tmp/aws_secret_access_key"
+    command = "cat /tmp/credentials | jq --exit-status --raw-output .Credentials.SecretAccessKey >> aws_secret_access_key"
   }
 
-  provisioner "local-exec" {
-    command = "cat /tmp/credentials | jq --exit-status --raw-output .Credentials.SessionToken >> /tmp/aws_session_token"
-  }
+  /*provisioner "local-exec" {
+    command = "cat /tmp/credentials | jq --exit-status --raw-output .Credentials.SessionToken >> aws_session_token"
+  }*/
 }
 
 data "null_data_source" "read_credentials" {
   inputs = {
-    aws_access_key_id     = "${file("/tmp/aws_access_key_id")}"
-    aws_secret_access_key = "${file("/tmp/aws_secret_access_key")}"
-    aws_session_token     = "${file("/tmp/aws_session_token")}"
+    aws_access_key_id     = "${file("aws_access_key_id")}"
+    aws_secret_access_key = "${file("aws_secret_access_key")}"
+    #aws_session_token     = "${file("aws_session_token")}"
   }
   depends_on = ["null_resource.write_credentials"]
 }
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "${data.null_data_source.read_credentials.aws_access_key_id}"
-  secret_key = "${data.null_data_source.read_credentials.aws_access_key_id}"
-  token      = "${data.null_data_source.read_credentials.aws_session_token}"
+  access_key = "${data.null_data_source.read_credentials.outputs[aws_access_key_id]}"
+  secret_key = "${data.null_data_source.read_credentials.outputs[aws_access_key_id]}"
+  #token      = "${data.null_data_source.read_credentials.outputs[aws_session_token]}"
 }
