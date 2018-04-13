69032ac39a10a1167002e693153bfd6e9873718c
Roger Berlind <rberlind@optonline.net>
2018-04-12 21:10:39 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,15 @@
 resource "null_resource" "write_credentials" {
+  
   provisioner "local-exec" {
-    command = "sudo apt-get -y install python python-pip jq"
+    command = "sudo apt-get update"
   }
   
   provisioner "local-exec" {
-    command = "sudo pip install awscli --upgrade --user"
+    command = "sudo apt-get -y install jq"
   }
+
+  provisioner "local-exec" {
+    command = "sudo apt-get -y install awscli"
 
   provisioner "local-exec" {
     command = "aws sts assume-role --role-arn=arn:aws:iam::128997349609:role/terraform-role --role-session-name='TFE-Apply' --region=us-east-1 >> json_credentials"
@@ -19,23 +23,33 @@ resource "null_resource" "write_credentials" {
     command = "cat json_credentials | jq --exit-status --raw-output .Credentials.SecretAccessKey >> aws_secret_access_key"
   }
 
-  /*provisioner "local-exec" {
+  provisioner "local-exec" {
     command = "cat json_credentials | jq --exit-status --raw-output .Credentials.SessionToken >> aws_session_token"
-  }*/
+  }
 }
 
 data "null_data_source" "read_credentials" {
   inputs = {
     aws_access_key_id     = "${file("aws_access_key_id")}"
     aws_secret_access_key = "${file("aws_secret_access_key")}"
-    #aws_session_token     = "${file("aws_session_token")}"
+    aws_session_token     = "${file("aws_session_token")}"
   }
   depends_on = ["null_resource.write_credentials"]
 }
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "${data.null_data_source.read_credentials.outputs[aws_access_key_id]}"
-  secret_key = "${data.null_data_source.read_credentials.outputs[aws_access_key_id]}"
-  #token      = "${data.null_data_source.read_credentials.outputs[aws_session_token]}"
+  access_key = "${data.null_data_source.read_credentials.outputs["aws_access_key_id"]}"
+  secret_key = "${data.null_data_source.read_credentials.outputs["aws_access_key_id"]}"
+  token      = "${data.null_data_source.read_credentials.outputs["aws_session_token"]}"
 }
+  
+resource "aws_instance" "web" {
+  ami           = "ami-2e1ef954"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "assumed_role_instance"
+  }
+}
+
